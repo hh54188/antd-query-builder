7d28de51e6ee615781bf2d14d0bdbfecc45c1d39
liguangyi <252438823@qq.com>
2018-01-22 23:43:24 +0800

    update

---
+++ src/QueryBuilder/Guideline/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './index.less'
+
+class Guideline extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div className="Guideline" />
+  }
+}
+
+export default Guideline
---
+++ src/QueryBuilder/Splitline/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './index.less'
+
+class Splitline extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div className="Splitline" />
+  }
+}
+
+export default Splitline
